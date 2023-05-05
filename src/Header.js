b2e78f924beffa806ced0ffc3db77a99554320aa
José Wilson de Lima <josewilson791@gmail.com>
2023-05-05 09:21:02 -0300

    :sparkles: feat: Componente Header.js

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import { FaFacebook, FaSearch, FaAlignJustify } from "react-icons/fa";
+
+export default function(){
+    return (
+        <div className="header">
+
+        <div className="headerLeft">
+            {/* Icons */}
+            <div className="logo-fb">
+            <FaFacebook />
+            </div>
+            <div className="search-fb">
+            <FaSearch />
+            </div>
+            <div className="menu-fb">
+            <FaAlignJustify />
+            </div>
+        </div>
+
+        <div className="headerRight">
+            <div className="plus-btn">
+            +
+            </div>
+        </div>
+
+        </div>
+    );
+}
