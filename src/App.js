e24f1c1497168154c02dc7f7e9efebbcc15affcb
José Wilson de Lima <josewilson791@gmail.com>
2023-05-05 10:16:44 -0300

    :lipstick: style: Estilização do Stories e campo de postagens do Feed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Header from './Header';
+import Stories from './Stories';
+import Horrorcat from './resources/Horrorcat.jpg';
+import { AiFillVideoCamera } from "react-icons/ai";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Stories />
+
+      <div className="feed">
+        <div className="feedForm">
+          <img src={Horrorcat} />
+          <input type="text" placeholder="No que você está persando ?"/>
+        </div>
+      </div>
+
+      <div className="feedIcons">
+        <div className="video">
+          
+        </div>
+      </div>
+
     </div>
   );
 }
