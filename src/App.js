2b615e0173e79a98889c126865d05977a55f1971
José Wilson de Lima <josewilson791@gmail.com>
2023-05-05 16:47:48 -0300

    :recycle: refactor: Organizando os componentes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import './App.css';
 import Header from './Header';
 import Stories from './Stories';
-import Horrorcat from './resources/Horrorcat.jpg';
-import { AiFillVideoCamera } from "react-icons/ai";
+import FeedForm from './FeedForm';
+import FeedPost from './FeedPost';
 
 function App() {
   return (
     <div className="App">
+
       <Header />
       <Stories />
-
-      <div className="feed">
-        <div className="feedForm">
-          <img src={Horrorcat} />
-          <input type="text" placeholder="No que você está persando ?"/>
-        </div>
-      </div>
-
-      <div className="feedIcons">
-        <div className="video">
-          
-        </div>
-      </div>
+      <FeedForm />
+      <FeedPost nome="Smooth" conteudo="Venha aprender line art só agora" horario="16h"/>
+      <FeedPost nome="Smooth" conteudo="Venha aprender line art só agora" horario="16h"/>
+      <FeedPost nome="Smooth" conteudo="Venha aprender line art só agora" horario="16h"/>
+      <FeedPost nome="Smooth" conteudo="Venha aprender line art só agora" horario="16h"/>
+      <FeedPost nome="Smooth" conteudo="Venha aprender line art só agora" horario="16h"/>
 
     </div>
   );
